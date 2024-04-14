8d1cae9cfe4514a9bfebf98d1bef1654306688e8
Damir Temir <temirdamir10@gmail.com>
2024-04-14 07:29:15 -0500

    Add logic for disp gif

--- components/quiz.jsx
+++ components/quiz.jsx
@@ -2,7 +2,26 @@
 import React, { useState, useEffect } from 'react';
 import quizData from '../public/quizData.json';
 import Results from './results';
+import Image from 'next/image';
 import question1 from '../public/question1.gif'
+import question2 from '../public/question2.gif'
+import question3 from '../public/question3.gif'
+import question4 from '../public/question4.gif'
+import question5 from '../public/question5.gif'
+import question6 from '../public/question6.gif'
+import question7 from '../public/question7.gif'
+import question8 from '../public/question8.gif'
+
+const questionGif = [
+    question1,
+    question2,
+    question3,
+    question4,
+    question5,
+    question6,
+    question7,
+    question8
+];
 
 function Quiz() {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -41,9 +60,13 @@ function Quiz() {
         return <Results maxScoreHackathon={maxScoreHackathon} />;
     }
 
+    const gifSrc = questionGif[currentQuestionIndex];
+    console.log(currentQuestionIndex);
+
     return (
         <div className="flex flex-col items-center justify-center p-4">
             <div className="bg-white p-4 rounded-lg shadow-md w-full max-w-md">
+                <Image src={gifSrc} alt={`Question ${currentQuestionIndex + 1}`}/>
                 <h2 className="text-lg font-bold mb-4 text-black">{quizData[currentQuestionIndex].question}</h2>
                 <div className="grid grid-cols-2 gap-4">
                     {quizData[currentQuestionIndex].options.map((option, index) => (
