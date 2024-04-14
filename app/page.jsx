12b0e5566dbf8294f898937c76ea392828a9e22b
Damir Temir <temirdamir10@gmail.com>
2024-04-13 23:41:32 -0500

    Reduce margin in h1

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <main className="flex flex-col gap-2 sm:gap-16">
             <section className="flex flex-col items-start gap-1 sm:gap-4">
-                <h1 className="mb-7">What Hackathon are You?</h1>
+                <h1 className="mb-0">What Hackathon are You?</h1>
                 {/* <p className="text-lg">You ever ask yourself, what should I do with my life?</p> */}
             </section>
             <Quiz />
