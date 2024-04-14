a4f20962a0aee5f21d2c342d51e8e54b70cc7ac1
Damir Temir <temirdamir10@gmail.com>
2024-04-14 07:02:09 -0500

    Retry build

--- components/results.jsx
+++ components/results.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import HackKU from '../public/HackKU.svg'
+import HackKU from '../public/HackKU'
 
 const hackathonImages = {
     HackKU
